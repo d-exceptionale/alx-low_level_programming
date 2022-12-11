a7dfc6cd336020b887818cc57f92137aa713bf79
Prince Victory <princeogbonnia6@gmail.com>
2022-12-11 14:50:31 -0800

    Task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 as the value if exited properly and a non-zero if otherwise
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
