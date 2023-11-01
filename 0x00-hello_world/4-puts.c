14dd6bc6acb2142013c11e98f8a54163fa785810
d-exceptional <princeogbonnia@gmail.com>
2023-11-01 11:27:13 -0700

    Task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints "programming is like building a multilingual puzzle
+ * followed by a new line
+ * Return: Always 0 (success!).
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
