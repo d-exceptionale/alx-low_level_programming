845bb4c40e24b9479a65d83c4a1335487d93be9b
d-exceptional <princeogbonnia@gmail.com>
2023-11-04 13:53:10 -0700

    Task 100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,21 @@ int main(void)
 
 	for (i = '0' ; i <= '9' ; i++)
 	{
-		for (j = '0' ; j <= '9' ; j++)
+		for (j = i + 1 ; j <= '9' ; j++)
 		{
-			putchar(i);
-			putchar(j);
-
-			if (i < '9' || j < '9')
+			if (i != j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+
+				if (!(i == '8' && j == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+					continue;
 			}
-			else
-				continue;
 		}
 	}
 
