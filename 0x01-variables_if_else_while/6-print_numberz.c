56aad7b1d6cf77389c6c8e22ad1eabf9dc63fcee
d-exceptional <princeogbonnia@gmail.com>
2023-11-04 00:17:46 -0700

    Task 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * followed by a new line.
+ * Return: always 0 (success).
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
