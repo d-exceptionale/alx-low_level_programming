5b0deb6e83fd8ec617918a044d3326ee5b29c694
d-exceptional <princeogbonnia@gmail.com>
2023-11-06 21:11:40 -0800

    Task 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -32,22 +32,23 @@ int main(void)
 
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
-																
 		for (index = 0; password[index]; index++)
-																				{
-																					if (password[index] >= (33 + diff_half1))
-																					{
-				password[index] -= diff_half1;																																	break;																																			}
-																				}
-																				for (index = 0; password[index]; index++)
-																				{
-																					if (password[index] >= (33 + diff_half2))																															{
-																						password[index] -= diff_half2;
-																						break;
-																					}
-																				}
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
 	}
- 
+
 	printf("%s", password);
 	return (0);
 }
