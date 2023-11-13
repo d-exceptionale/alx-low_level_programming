706de65e666ca7fae13f73025a4ef52334251189
d-exceptional <princeogbonnia@gmail.com>
2023-11-13 11:14:18 -0800

    Task 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,50 @@
 #include "main.h"
 
 /**
- * _strcmp - prints a function that compares two strings
- * @s1: first string to be compared to s2
- * @s2: second string to be compared
- * Return: -15 if s1 < s2, 0 if s1 == s2 and 15 if s1 > s2.
- */
-
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1 = 0, len_s2 = 0, i, j, retval;
+	int a = 0, b = 0, c = 0, r = 0, lim;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		len_s1++;
-	for (j = 0; s2[j] != '\0'; j++)
-		len_s2++;
-	if (len_s1 < len_s2)
-		retval = -15;
-	else if (len_s1 > len_s2)
-		retval = 15;
+	while (s1[a])
+	{
+		a++;
+	}
+
+	while (s2[b])
+	{
+		b++;
+	}
+
+	if (a <= b)
+	{
+		lim = a;
+	}
 	else
-		retval = 0;
-	return (retval);
+	{
+		lim = b;
+	}
+
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
 }
