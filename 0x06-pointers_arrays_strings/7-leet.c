0ae57a069b8faf71f7d07518553a4722adf6b4f7
d-exceptional <princeogbonnia@gmail.com>
2023-11-16 13:30:35 -0800

    Task 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337.
+ * @str: string to be encoded.
+ * Return: pointer to encoded string.
+ */
+
+char *leet(char *str)
+{
+	int a = 0, b;
+	char d_str[] = {'a', 'e', 'o', 't', 'l'};
+	char d_code[] = {'4', '3', '0', '7', '1'};
+
+	while (str[a])
+	{
+		b = 0;
+		while (b < 0)
+		{
+			if (str[a] == d_str[b] || str[a] + 32 == d_str[b])
+				str[a] = d_code[b];
+			b++;
+		}
+
+		a++;
+	}
+
+	return (str);
+}
