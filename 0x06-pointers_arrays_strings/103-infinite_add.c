ef367cfe7a2c483a6b213edb4e4db7bc716b5bdd
d-exceptional <princeogbonnia@gmail.com>
2023-11-18 18:33:24 -0800

    Task 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two numbers.
+ * @n1: first number to be added
+ * @n2: second number to be added
+ * @r: buffer used to store the results.
+ * @size_r: buffer size.
+ * Return: pointer to the results (r).
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int
+}
