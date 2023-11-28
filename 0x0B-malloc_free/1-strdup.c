26a1121b8a73eb5d8e7f210673787f1bbe24ebb2
d-exceptional <princeogbonnia@gmail.com>
2023-11-27 20:45:01 -0800

    Task 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *   _strcpy - Copy a string
+ *   @dest: Destination value
+ *   @src: Source value
+ *   Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - returns a pointer to newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: str to be copied
+ * Return: NULL if there's no string, pointer to duplicated string on success
+ * and NULL if insufficient memory was available.
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *mstr;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str != '\0'; i++)
+		str++;
+
+	mstr = (char *)malloc(i * sizeof(char));
+
+	if (mstr == NULL)
+		return (NULL);
+
+	_strcpy(mstr, str);
+	return (mstr);
+
+
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,7 @@
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
 
 #endif
