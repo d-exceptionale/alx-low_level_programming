a40295d701ade014fbcce68833caeac695a1fde4
d-exceptional <princeogbonnia@gmail.com>
2023-11-27 18:38:28 -0800

    Task 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	p = (char*)malloc(size * sizeof(char));
+	p = (char *)malloc(size * sizeof(char));
 
 	if (p == NULL)
 	{
@@ -27,7 +27,7 @@ char *create_array(unsigned int size, char c)
 
 	for (i = 0; i < size; i++)
 	{
-		p[i] = c
+		p[i] = c;
 	}
 
 	return (p);
