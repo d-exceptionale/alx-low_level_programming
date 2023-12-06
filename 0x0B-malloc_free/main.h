1cf8c55e249417e1e0315000beefe7d5e1c44604
d-exceptional <princeogbonnia@gmail.com>
2023-12-06 15:55:39 -0800

    Task 101-strtow.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,6 @@ void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
