c6dbf134ea84687772a4c9b6a9e2366ebeaf5f8d
d-exceptional <princeogbonnia@gmail.com>
2023-12-18 02:06:53 -0800

    Task 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", (*d).name);
 
 		if (!((*d).age))
-			printf("Age: (nil)\n");
+			printf("Age: 0\n");
 		else
 			printf("Age: %f\n", (*d).age);
 
