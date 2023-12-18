fa51cd830f58daf4add1f846b7c3cba93ae566c2
d-exceptional <princeogbonnia@gmail.com>
2023-12-18 01:03:22 -0800

    Task 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -11,11 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
-	return (d);
 }
