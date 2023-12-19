5ead7f5dbc1acd62b3354636c3c79ea5cbd17650
d-exceptional <princeogbonnia@gmail.com>
2023-12-19 02:08:30 -0800

    Task 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,6 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		n_dog->owner = malloc(sizeof(char) * size_owner);
 		if (n_dog->owner == NULL)
 		{
+			free(n_dog->name);
 			free(n_dog);
 			return (NULL);
 		}
