ec4e6912ca44fc0a5d5b0fbff976b1f118affe4f
d-exceptionale <princeogbonnia6@gmail.com>
2024-02-18 05:51:09 -0800

    Task 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -17,18 +17,10 @@ int pop_listint(listint_t **head)
 	if ((*head) == NULL)
 		return (0);
 
-	if ((*head)->next == NULL)
-	{
-		val = (*head)->n;
-		free(*head);
-		return (val);
-	}
-	else
-	{
-		temp = *head;
-		val = (*head)->n;
-		*head = (*head)->next;
-		free(temp);
-		return (val);
-	}
+	temp = *head;
+	val = temp->n;
+	*head = (*head)->next;
+	free(temp);
+
+	return (val);
 }
