a3992dd8ae8f492ab37c7575c3ed4d63225f612a
d-exceptionale <princeogbonnia6@gmail.com>
2024-03-03 00:11:46 -0800

    Task gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-print_binary.c _putchar.c -o b

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed in binary
+ * Return: nothing,just print binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shift;
+
+	if (n == 0)
+		printf("0");
+
+	for (temp = n, shift = 0; (temp >>= 1) > 0; shift++)
+		;
+
+	while (shift >= 0)
+	{
+		if ((n >> shift) & 1)
+			printf("1");
+		else
+			printf("0");
+		shift--;
+	}
+}
