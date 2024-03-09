572e95fda8206b3e4b0877881ef5c6c9923db362
d-exceptionale <princeogbonnia6@gmail.com>
2024-03-09 02:52:10 -0800

    Task 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,10 +15,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 
-	for (hold = 1; index > 0; index--, hold*=2)
+	for (hold = 1; index > 0; index--, hold *= 2)
 		;
 
-	*n+=hold;
+	*n += hold;
 
 	return (1);
 
